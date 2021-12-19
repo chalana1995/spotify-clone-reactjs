3e473c143a4e24f13a39b1d40341c8850bec72a1
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-12-20 00:49:52 +0530

    today work done

--- spotify-clone/components/Sidebar.js
+++ spotify-clone/components/Sidebar.js
@@ -7,19 +7,35 @@ import {
 
 function Sidebar() {
   return (
-    <div>
-      <button>
-        <HomeIcon className="w-5 h-5" />
-        <p>Home</p>
-      </button>
-      <button>
-        <SearchIcon className="w-5 h-5" />
-        <p>Search</p>
-      </button>
-      <button>
-        <LibraryIcon className="w-5 h-5" />
-        <p>Your library</p>
-      </button>
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="w-5 h-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="w-5 h-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="w-5 h-5" />
+          <p>Your library</p>
+        </button>
+        <hr className="border-t-[0.1]  border-gray-900" />
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="w-5 h-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="w-5 h-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="w-5 h-5" />
+          <p>Your library</p>
+        </button>
+        <hr className="border-t-[0.1]  border-gray-900" />
+      </div>
     </div>
   );
 }

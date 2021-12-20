8cd146f79004702972cfbc2ef74f750c7c1db726
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-12-20 23:03:24 +0530

    side bar finish

--- spotify-clone/components/Sidebar.js
+++ spotify-clone/components/Sidebar.js
@@ -3,12 +3,14 @@ import {
   SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
 } from "@heroicons/react/outline";
 
 function Sidebar() {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
-      <div>
+      <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="w-5 h-5" />
           <p>Home</p>
@@ -23,18 +25,20 @@ function Sidebar() {
         </button>
         <hr className="border-t-[0.1]  border-gray-900" />
         <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="w-5 h-5" />
-          <p>Home</p>
+          <PlusCircleIcon className="w-5 h-5" />
+          <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <SearchIcon className="w-5 h-5" />
-          <p>Search</p>
+          <HeartIcon className="w-5 h-5" />
+          <p>Liked songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="w-5 h-5" />
-          <p>Your library</p>
+          <RssIcon className="w-5 h-5" />
+          <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1]  border-gray-900" />
+
+        <p className="cursor-pointer hover:text-white">Play list name</p>
       </div>
     </div>
   );
